fd7b525656442e7dea207b9bb4dc4130c278145c
Max Orange <mrmask@mail.ru>
2022-08-04 08:20:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,11 @@ def generate(starting_text):
 
 txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
 out = grad.Textbox(lines=6, label="Generated Text")
-examples = [["mythology of the Slavs"], ["All-seeing eye monitors these world"], ["astronaut dog"], 
-            ["A monochrome forest of ebony trees"], ["sad view of worker in office,"], 
+examples = [["mythology of the Slavs"], ["All-seeing eye monitors these world"], ["astronaut dog"],
+            ["A monochrome forest of ebony trees"], ["sad view of worker in office,"],
             ["Headshot photo portrait of John Lennon"], ["wide field with thousands of blue nemophila,"]]
-title = "Prompt Generator"
+title = "Midjourney Prompt Generator"
+description = "This is an unofficial demo for Midjourney Prompt Generator. To use it, simply send your text, or click one of the examples to load them. Read more at the links below.<br>Model: https://huggingface.co/succinctly/text2image-prompt-generator<br>Telegram bot: https://t.me/prompt_generator_bot<br>[![](https://img.shields.io/twitter/follow/DoEvent?label=@DoEvent&style=social)](https://twitter.com/DoEvent)"
 article = "<div><center><img src='https://visitor-badge.glitch.me/badge?page_id=max_skobeev_prompt_generator_public' alt='visitor badge'></center></div>"
 
 grad.Interface(fn=generate,
@@ -43,6 +44,7 @@ grad.Interface(fn=generate,
                outputs=out,
                examples=examples,
                title=title,
+               description=description,
                article=article,
                allow_flagging='never',
                cache_examples=False,
