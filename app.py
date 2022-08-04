8d7467554b1a0a171ace40b5d7ccf97eb6772de7
Max Orange <mrmask@mail.ru>
2022-08-04 03:16:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from transformers import pipeline, set_seed
 import gradio as grad
 import random
 
-
 gpt2_pipe = pipeline('text-generation', model='succinctly/text2image-prompt-generator')
 
 
 def generate(starting_text):
-    seed = random.randint(1, 10000000)
+    seed = random.randint(1, 100000)
     set_seed(seed)
-    response= gpt2_pipe(starting_text, max_length=20, num_return_sequences=5)
-    return response[1]['generated_text']
-    
-txt=grad.Textbox(lines=1, label="English", placeholder="English Text here")
-out=grad.Textbox(lines=1, label="Generated Text")
+    while True:
+        response = str(gpt2_pipe(starting_text, max_length=30, num_return_sequences=random.randint(5, 15))).strip()
+        if starting_text != response[1]['generated_text']:
+            print(f"Repeat: {response}")
+        else:
+            return response[1]['generated_text']
 
-grad.Interface(fn=generate, inputs=txt, outputs=out, 
-             allow_flagging='never',
-             cache_examples=False,
-             theme="default").launch(enable_queue=True, debug=True)
+
+txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
+out = grad.Textbox(lines=1, label="Generated Text")
+
+grad.Interface(fn=generate, inputs=txt, outputs=out,
+               allow_flagging='never',
+               cache_examples=False,
+               theme="default").launch(enable_queue=True, debug=True)
