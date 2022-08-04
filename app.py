926e92f729feb6c05f70b71053409d43e1bb5124
Max Orange <mrmask@mail.ru>
2022-08-04 04:05:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def generate(starting_text):
     response = gpt2_pipe(starting_text, max_length=random.randint(20, 45), num_return_sequences=random.randint(5, 15))
     response_list = []
     for x in response:
-        if x['generated_text'].strip() != starting_text:
+        if x['generated_text'].strip() != starting_text and len(x['generated_text'].strip()) > (len(starting_text) + 4):
             response_list.append(x['generated_text'])
 
     response_end = "\n".join(response_list)
@@ -31,10 +31,14 @@ def generate(starting_text):
 
 txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
 out = grad.Textbox(lines=5, label="Generated Text")
+title = "Prompt Generator"
+article = "<div><center><img src='https://visitor-badge.glitch.me/badge?page_id=max_skobeev_prompt_generator_public' alt='visitor badge'></center></div>"
 
 grad.Interface(fn=generate,
                inputs=txt,
                outputs=out,
+               title=title,
+               article=article,
                allow_flagging='never',
                cache_examples=False,
                theme="default").launch(enable_queue=True, debug=True)
