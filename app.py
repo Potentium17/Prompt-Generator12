871bae2a45f4b5f0c56279a3c0a69332322ee0d2
Max Orange <mrmask@mail.ru>
2022-08-04 03:34:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,20 @@ def generate(starting_text):
     seed = random.randint(1, 100000)
     set_seed(seed)
     response = gpt2_pipe(starting_text, max_length=30, num_return_sequences=random.randint(5, 15))
-    response_list = "\n".join(response)
-    return response_list
+    response_list = []
+    for x in response:
+        if x['generated_text'].strip() != starting_text:
+            response_list.append(x['generated_text'])
+        
+    response_end = "\n".join(response_list)
+    return response_end
 
 
 txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
 out = grad.Textbox(lines=5, label="Generated Text")
 
 grad.Interface(fn=generate,
-               inputs=txt, 
+               inputs=txt,
                outputs=out,
                allow_flagging='never',
                cache_examples=False,
