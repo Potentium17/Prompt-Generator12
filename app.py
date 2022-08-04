3b250809c242f38ac8d101013e0fd14a9b451b43
Max Orange <mrmask@mail.ru>
2022-08-04 03:42:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from transformers import pipeline, set_seed
 import gradio as grad
 import random
+import re
 
 gpt2_pipe = pipeline('text-generation', model='succinctly/text2image-prompt-generator')
+
+with open("name.txt", "r") as f:
+    line = f.readlines()
 
 
 def generate(starting_text):
     seed = random.randint(1, 100000)
     set_seed(seed)
-    response = gpt2_pipe(starting_text, max_length=30, num_return_sequences=random.randint(5, 15))
+    
+    # If the text field is empty
+    if starting_text == "":
+        starting_text: str = line[random.randrange(0, len(line))]
+        starting_text: str = re.sub(r"[,:\-–.!;?_]", '', starting_text)
+        print(starting_text)
+        
+    response = gpt2_pipe(starting_text, max_length=random.randint(20, 45), num_return_sequences=random.randint(5, 15))
     response_list = []
     for x in response:
         if x['generated_text'].strip() != starting_text:
             response_list.append(x['generated_text'])
-        
+
     response_end = "\n".join(response_list)
     return response_end
 
