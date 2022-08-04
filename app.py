9133fa2ebe40138d5f0904f096eff28a289cdbb5
Max Orange <mrmask@mail.ru>
2022-08-04 03:27:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,17 @@ gpt2_pipe = pipeline('text-generation', model='succinctly/text2image-prompt-gene
 def generate(starting_text):
     seed = random.randint(1, 100000)
     set_seed(seed)
-    while True:
-        response = gpt2_pipe(starting_text, max_length=30, num_return_sequences=random.randint(5, 15))
-        if starting_text != response[1]['generated_text'].strip():
-            print(f"Repeat: {response}")
-        else:
-            return response[1]['generated_text']
+    response = gpt2_pipe(starting_text, max_length=30, num_return_sequences=random.randint(5, 15))
+    response_list = "\n".join(response)
+    return response_list
 
 
 txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
-out = grad.Textbox(lines=1, label="Generated Text")
+out = grad.Textbox(lines=5, label="Generated Text")
 
-grad.Interface(fn=generate, inputs=txt, outputs=out,
+grad.Interface(fn=generate,
+               inputs=txt, 
+               outputs=out,
                allow_flagging='never',
                cache_examples=False,
                theme="default").launch(enable_queue=True, debug=True)
