875c3803d7ec2f460d4cbbe601105b056656d7b1
Max Orange <mrmask@mail.ru>
2022-08-13 17:19:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,24 +10,28 @@ with open("name.txt", "r") as f:
 
 
 def generate(starting_text):
-    seed = random.randint(1, 100000)
-    set_seed(seed)
-
-    # If the text field is empty
-    if starting_text == "":
-        starting_text: str = line[random.randrange(0, len(line))].replace("\n", "").lower().capitalize()
-        starting_text: str = re.sub(r"[,:\-–.!;?_]", '', starting_text)
-        print(starting_text)
-
-    response = gpt2_pipe(starting_text, max_length=random.randint(20, 45), num_return_sequences=random.randint(5, 15))
-    response_list = []
-    for x in response:
-        resp = x['generated_text'].strip()
-        if resp != starting_text and len(resp) > (len(starting_text) + 4) and resp.endswith((":", "-", "—")) is False:
-            response_list.append(resp)
-
-    response_end = "\n".join(response_list)
-    return response_end
+    for count in range(6):
+        seed = random.randint(100, 1000000)
+        set_seed(seed)
+    
+        # If the text field is empty
+        if starting_text == "":
+            starting_text: str = line[random.randrange(0, len(line))].replace("\n", "").lower().capitalize()
+            starting_text: str = re.sub(r"[,:\-–.!;?_]", '', starting_text)
+            print(starting_text)
+    
+        response = gpt2_pipe(starting_text, max_length=random.randint(20, 45), num_return_sequences=random.randint(5, 15))
+        response_list = []
+        for x in response:
+            resp = x['generated_text'].strip()
+            if resp != starting_text and len(resp) > (len(starting_text) + 4) and resp.endswith((":", "-", "—")) is False:
+                response_list.append(resp)
+    
+        response_end = "\n".join(response_list)
+        if response_end != "":
+            return response_end
+        if count == 5:
+            return response_end
 
 
 txt = grad.Textbox(lines=1, label="English", placeholder="English Text here")
